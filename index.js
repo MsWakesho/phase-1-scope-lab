afdba4e76263cda2accfac005a3c2b84e751faff
MsWakesho <wakeshosonja@gmail.com>
2023-12-16 20:54:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+
+var customerName = "bob"
+
+
+function upperCaseCustomerName (){
+    window.customerName = window.customerName.toUpperCase()
+} // this modifies the function to uppercase
+  // the window. gives access to the global scope.
+
+
+function setBestCustomer(){
+    window.bestCustomer = "not bob"
+
+}
+
+function overwriteBestCustomer(){
+    window.bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "melissa"
+
+function changeLeastFavoriteCustomer (){
+
+    leastFavoriteCustomer = "harry "
+}
+
+
